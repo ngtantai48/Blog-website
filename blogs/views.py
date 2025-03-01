36a497550db7a4d781331cee013c6ae93691441a
NguyenTanTai <ngtantai48@gmail.com>
2025-03-01 23:53:07 +0700

    - utilizing context processors

--- blogs/views.py
+++ blogs/views.py
@@ -7,13 +7,13 @@ def posts_by_category(request, category_id):
     posts = Blogs.objects.filter(status="published", category=category_id)
 
     # use try/except when we want to do some custom action if the category does not exist
-    # try:
-    #     category = Category.objects.get(pk=category_id)
-    # except:
-    #     return redirect('home')
+    try:
+        category = Category.objects.get(pk=category_id)
+    except:
+        return redirect('home')
 
     # use get_object_or_404 when you want to show 404 error page if the category does not exist
-    category = get_object_or_404(Category, pk=category_id)
+    # category = get_object_or_404(Category, pk=category_id)
 
     context = {
         "posts": posts, 
