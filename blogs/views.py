c2fa6bd1b840e0e20de15b58bedba7c875be675d
NguyenTanTai <ngtantai48@gmail.com>
2025-03-02 01:40:45 +0700

    + func: search

--- blogs/views.py
+++ blogs/views.py
@@ -1,6 +1,6 @@
-from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Blogs, Category
+from django.db.models import Q
 
 
 def posts_by_category(request, category_id):
@@ -11,7 +11,7 @@ def posts_by_category(request, category_id):
     try:
         category = Category.objects.get(pk=category_id)
     except:
-        return redirect('home')
+        return redirect("home")
 
     # use get_object_or_404 when you want to show 404 error page if the category does not exist
     # category = get_object_or_404(Category, pk=category_id)
@@ -25,4 +25,23 @@ def posts_by_category(request, category_id):
 
 # blogs
 def blogs(request, slug):
-    return render(request, 'blogs.html')
+    single_post = get_object_or_404(Blogs, slug=slug, status='published')
+    context = {
+        'single_post': single_post
+    }
+    return render(request, "blogs.html", context)
+
+
+# search dunctionality
+def search(request):
+    keyword = request.GET.get('keyword')
+    blogs = Blogs.objects.filter(Q(title__icontains=keyword) 
+                                | Q(short_description__icontains=keyword) 
+                                | Q(blog_body__icontains=keyword),
+                                status='published'
+    )
+    content={
+        'blogs': blogs,
+        'keyword': keyword
+    }
+    return render(request, 'search.html', content)
