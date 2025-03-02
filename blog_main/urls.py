9ae59fa268ee66882630a3a39fef2d0630449ab6
NguyenTanTai <ngtantai48@gmail.com>
2025-03-02 16:42:04 +0700

    +func: login & logout

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -13,4 +13,6 @@ urlpatterns = [
     path("blogs/<slug:slug>/", BlogsView.blogs, name="blogs"),
     path("blogs/search/", BlogsView.search, name="search"),
     path("register/", views.register, name="register"),
+    path("login/", views.login, name='login'),
+    path("logout/", views.logout, name='logout')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blog_main/views.py
+++ blog_main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from blogs.models import Category, Blogs
 from .forms import RegistrationForm
+from django.contrib import auth
+from django.contrib.auth.forms import AuthenticationForm
 
 
 def home(request):
@@ -29,3 +31,30 @@ def register(request):
         'form': form
     }
     return render(request, 'register.html', context)
+
+
+# Login
+def login(request):
+    if request.method=="POST":
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            
+            user = auth.authenticate(username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+                return redirect('home')
+    else:
+        form = AuthenticationForm()
+    
+    context = {
+        'form': form
+    }
+    return render(request, 'login.html', context)
+
+
+# Logout
+def logout(request):
+    auth.logout(request)
+    return redirect('home')
