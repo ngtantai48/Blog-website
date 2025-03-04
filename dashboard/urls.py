28497ef794996197435482cfc3db3faa38a19e4a
NguyenTanTai <ngtantai48@gmail.com>
2025-03-05 01:34:48 +0700

    - add new category

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.dashboard, name="dashboard")
+    path("", views.dashboard, name="dashboard"),
+    path("categories/", views.categories, name="categories"),
+    path("categories/add", views.add_categories, name="add_categories"),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,17 +1,39 @@
-from django.shortcuts import render
-from blogs.models import Category, Blogs
+from django.shortcuts import redirect, render
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from blogs.models import Category, Blogs
+from dashboard.forms import CategoryForm
 
 
-# Create your views here.
-@login_required(login_url='login')
+@login_required(login_url="login")
 def dashboard(request):
     category_counts = Category.objects.all().count()
     blogs_counts = Blogs.objects.all().count()
 
     context = {
-        'category_counts': category_counts,
-        'blogs_counts': blogs_counts
+        "category_counts": category_counts, 
+        "blogs_counts": blogs_counts
     }
 
     return render(request, "dashboard/dashboard.html", context)
+
+
+def categories(request):
+    return render(request, "dashboard/categories.html")
+
+
+def add_categories(request):
+    if request.method == "POST":
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Category created successfully!")
+            return redirect("categories")
+        else:
+            messages.error(request, "Failed to create category. Please check the form.")
+    form = CategoryForm()
+
+    context = {
+        "form": form
+    }
+    return render(request, "dashboard/add_categories.html", context)
